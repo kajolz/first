a5eaef97e6eac544ed18f054aa07c8b5be5f3178
kajolz <53860138+kajolz@users.noreply.github.com>
2019-08-14 02:24:02 -0700

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,22 @@
+e=eval(input('enter english marks'))
+h=eval(input('enter hindi marks'))
+m=eval(input('enter maths marks'))
+s=eval(input('enter science marks'))
+g=eval(input('enter geography marks'))
+ma=((e+h+m+s+g)/500)*100
+if ma>75:
+    print('o grade')
+else:
+    if ma>=60 and ma<75:
+        print('a grade')
+    else:
+        if ma>=45 and ma<60:
+            print('b grade')
+        else:
+            if ma>=35 and ma<45:
+                print('c grade')
+            else:
+                if ma<35:
+                    print('fail')
+                    
+                     
